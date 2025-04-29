29c794d7b0b2dfc5c119830d8ac52aa79577d6e7
Viktor <67437+vfonic@users.noreply.github.com>
2025-04-29 20:46:20 -0300

    Fix: Allow using as: :hotwire_combobox by passing :collection param, w/o relation model

--- lib/activeadmin_hotwire_combobox_filters/inputs/hotwire_combobox_input.rb
+++ lib/activeadmin_hotwire_combobox_filters/inputs/hotwire_combobox_input.rb
@@ -2,10 +2,13 @@ module ActiveAdmin
   module Inputs
     class HotwireComboboxInput < Formtastic::Inputs::SelectInput
       def select_html
-        path = template.public_send(:"combobox_search_admin_#{reflection.klass.name.underscore.pluralize}_path")
+        path_or_collection = if reflection.present?
+                               template.public_send(:"combobox_search_admin_#{reflection.klass.name.underscore.pluralize}_path")
+                             else
+                               collection
+                             end
 
-        template.render(partial: "activeadmin_hotwire_combobox_filters/combobox_styles") <<
-          builder.combobox(input_name, path)
+        template.render(partial: "activeadmin_hotwire_combobox_filters/combobox_styles") << builder.combobox(input_name, path_or_collection)
       end
     end
   end
